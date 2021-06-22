a0c17391274f274c2fc0f77341c01b16299dba45
tmu <t.man.atl.2012@gmail.com>
2021-06-22 11:40:19 -0700

    finished addressing modes

--- address.c
+++ address.c
@@ -9,21 +9,30 @@
 
 #include "optcode.h"
 
+//boolean for size of index X/Y reg.s 1 if 8b, 0 if 16b
+static inline BYTE getX() {
+  return P & 0x10;
+}
 
-//TODO CONFIRM THAT ALL REGS ARE CORRECT SIZE 
+/* NOTE ABOUT REGISTER SIZE 
+ *
+ * All of the addressing modes that use X/Y registers must check 
+ * 8b or 16b current size 
+ * This means there is an inherent branch operation. 
+ * This seems ineffiecient, but I can't see any way around it
+ */
 
+//confirm whether this happens before or after PC gets incremented. 
 ADDRESS immediate (void) {//# 
-  //TODO make 8b/16b switch 
   return (((ADDRESS)PB << 16) | PC) + 1;
 }
 
-//check that bitshift without cast is safe 
 ADDRESS absolute (BYTE i, BYTE j) { //a
-  return (DB << 16) | (j << 8) | (i);
+  return ((ADDRESS)DB << 16) | (j << 8) | (i);
 }
 
 ADDRESS absoluteLong (BYTE i, BYTE j, BYTE k) { //al
-  return (k << 16) | (j << 8) | (i);
+  return ((ADDRESS)k << 16) | (j << 8) | (i);
 }
 
 ADDRESS direct (BYTE i) { //d
@@ -31,35 +40,35 @@ ADDRESS direct (BYTE i) { //d
 }
 
 ADDRESS directIndirectIndexed (BYTE i) { //(d),y
-  return (((((ADDRESS)D) + i) & 0xFFFF ) | (DB << 16) ) + Y;
+  return (((((ADDRESS)D) + i) & 0xFFFF ) | (DB << 16) ) + ((getX())? (Y & 0xFF):(Y)); //8b/16b switch.
 }
 
 ADDRESS directIndirectIndexedLong (BYTE i) { //[d],y
-  return ((((ADDRESS)D) + i) & 0xFFFF ) + Y;
+  return ((((ADDRESS)D) + i) & 0xFFFF ) + ((getX())? (Y & 0xFF):(Y));
 }
 
 ADDRESS directIndexedIndirect (BYTE i) { //(d,x)
-  return (((((ADDRESS)D) + i) + X) & 0xFFFF ) | (DB << 16);
+  return (((((ADDRESS)D) + i) + ((getX())? (X & 0xFF):(X)) )  & 0xFFFF ) | (DB << 16);
 }
 
 ADDRESS directIndexedWithX (BYTE i) { //d,x
-  return ((((ADDRESS)D) + i) + X) & 0xFFFF;
+  return ((((ADDRESS)D) + i) + ((getX())? (X & 0xFF):(X))) & 0xFFFF;
 }
 
 ADDRESS directIndexedWithY (BYTE i) { //d,y
-  return ((((ADDRESS)D) + i) + Y) & 0xFFFF;
+  return ((((ADDRESS)D) + i) + ((getX())? (Y & 0xFF):(Y))) & 0xFFFF;
 }
 
 ADDRESS absoluteIndexedWithX (BYTE i, BYTE j) { //a,x
-  return ((((ADDRESS)D) << 16) | (j << 8) | i) + X;
+  return ((((ADDRESS)D) << 16) | (j << 8) | i) + ((getX())? (X & 0xFF):(X));
 }
 
 ADDRESS absoluteIndexedWithY (BYTE i, BYTE j) { //a,y
-  return ((((ADDRESS)D) << 16) | (j << 8) | i) + Y;
+  return ((((ADDRESS)D) << 16) | (j << 8) | i) + ((getX())? (Y & 0xFF):(Y));
 }
 
 ADDRESS absoluteLongIndexedWithX (BYTE i, BYTE j, BYTE k) { //al,x
-  return ((((ADDRESS)k) << 16) | (j << 8) | i) + X;
+  return ((((ADDRESS)k) << 16) | (j << 8) | i) + ((getX())? (X & 0xFF):(X));
 }
 
 //confirm off by one error. PC inc happens BEFORE this 
@@ -72,44 +81,44 @@ ADDRESS programCounterRelativeLong (BYTE i, BYTE j) { //rl
   return (ADDRESS)(PC + (((WORD)j) << 8) + i); //this one is not signed 
 }
 
-//TODO confirm how this works. see reference txt mode 17. Check another source 
+//TODO number of bytes deref'd depends on instruction 
 ADDRESS absoluteIndirect (BYTE i, BYTE j) { //(a)
   return  *((ADDRESS*)(&memory[(((ADDRESS)j) << 16 ) | i]) ) & 0xFFFFFF; 
   //deref passed ptr in bank 00, take 24 bits from that location 
 }
 
-//same as above, confirm 
 ADDRESS directIndirect (BYTE i) { //(d)
   return (*((ADDRESS*)(&memory[(((ADDRESS)D) + i) & 0xFFFF]) ) & 0xFFFF) | (((ADDRESS)DB) << 16); 
   //construct ptr in bank 0, deref, take 16b and return in DB
 }
 
-//TODO confirm how this is different from absoluteIndirect 
-//I *think* that it is directIndirect but without DB, but I am not sure 
+//this is above but but pull all 3 bytes, not just 2 + DB
 ADDRESS directIndirectLong (BYTE i) { //[d]
-  return *((ADDRESS*)(&memory[(((ADDRESS)D) + i) & 0xFFFF]) ) & 0xFFFF; 
-  //same as above but in bank 0
+  return *((ADDRESS*)(&memory[(((ADDRESS)D) + i) & 0xFFFF]) ) & 0xFFFFFF; 
 }
 
+//TODO confirm if I need to deref this location. see mode 20
 ADDRESS absoluteIndexedIndirect (BYTE i, BYTE j) { //(a,x)
-  return (((((ADDRESS)j) << 8) | i ) + X ) & 0xFFFF;
+  return *((ADDRESS*)&memory[(((((ADDRESS)j) << 8) | i ) & 0xFFFF + ((getX())? (X & 0xFF):(X)) ) & 0xFFFF]) & 0xFFFF;
 }
 
+//TODO check stack conventions for SNES 
 ADDRESS stackRelative (BYTE i) { //d,s
   return (((ADDRESS)S) + i) & 0xFFFF; 
   //i is unsigned, can only reference things above the stack. Which direction does the SNES stack grow?
-  //TODO check stack conventions for SNES 
 }
 
+//TODO confirm deref? 
 ADDRESS stackRelativeIndirectIndexed (BYTE i) { //(d,s),y
-  return (((((ADDRESS)S) + i) & 0xFFFF) | (((ADDRESS)DB) << 16)) + Y;
+  return ((*((ADDRESS*)&memory[(((ADDRESS)S) + i) & 0xFFFF]) & 0xFFFF) | (((ADDRESS)DB) << 16)) + ((getX())? (Y & 0xFF):(Y));
 }
 
+//TODO confirm DB assignment 
 ADDRESS[2] blockSourceBankDestinationBank (BYTE i, BYTE j) { //xyc
   ADDRESS out[2];
-  out[1] = ((ADDRESS)X) | (((ADDRESS)j) << 16); //source 
+  out[1] = ((ADDRESS)((getX())? (X & 0xFF):(X))) | (((ADDRESS)j) << 16); //source 
   //NEEDS TO ASSIGN TO DB, should I do that here? 
   DB = i;
-  out[0] = ((ADDRESS)Y) | (((ADDRESS)DB) << 16); //destination 
+  out[0] = ((ADDRESS)((getX())? (Y & 0xFF):(Y))) | (((ADDRESS)DB) << 16); //destination 
   return out;
 }
