dc619f79eac6aad833ab270535d33bb9b65bc045
TCCQ <t.man.atl.2012@gmail.com>
2021-07-21 12:14:10 -0700

    opt-codes part 2

--- optcodes80-BF.c
+++ optcodes80-BF.c
@@ -9,133 +9,313 @@
 #include "memory.h"
 
 //BNE PC relative 
-void opt80 (BYTE);
+void opt80 (BYTE i) {
+  BNE(programCounterRelative(i));
+}
+
 //STA direct indexed indirect X 
-void opt81 (BYTE);
+void opt81 (BYTE i) {
+  STA(directIndexedIndirect(i));
+}
+
 //BRL PC relative long
-void opt82 (BYTE, BYTE);
+void opt82 (BYTE i, BYTE j) {
+  BRL(programCounterRelativeLong(i,j));
+}
+
 //STA stack relative 
-void opt83 (BYTE);
+void opt83 (BYTE i) {
+  STA(stackRelative(i));
+}
+
 //STY direct 
-void opt84 (BYTE);
+void opt84 (BYTE i) {
+  STY(direct(i));
+}
+
 //STA direct 
-void opt85 (BYTE);
+void opt85 (BYTE i) {
+  STA(direct(i));
+}
+
 //STX direct 
-void opt86 (BYTE);
+void opt86 (BYTE i) {
+  STX(direct(i));
+}
+
 //STA direct indirect long 
-void opt87 (BYTE);
+void opt87 (BYTE i) {
+  STA(directIndirectLong(i));
+}
+
 //DEY impied
-void opt88 (void);
+void opt88 (void) {
+  DEY();
+}
+
 //BIT immediate 
-void opt89 (BYTE, BYTE);
+void opt89 (BYTE i, BYTE j);
+
 //TXA implied 
-void opt8A (void);
+void opt8A (void) {
+  TXA();
+}
+
 //PHB stack :
-void opt8B (void);
+void opt8B (void) {
+  PHB();
+}
+
 //STY absolute
-void opt8C (BYTE, BYTE);
+void opt8C (BYTE i, BYTE j) {
+  STY(absolute(i,j));
+}
+
 //STA absolute 
-void opt8D (BYTE, BYTE);
+void opt8D (BYTE i, BYTE j) {
+  STA(absoltue(i,j));
+}
+
 //STX absoltue
-void opt8E (BYTE, BYTE);
+void opt8E (BYTE i, BYTE j) {
+  STX(absolute(i,j));
+}
+
 //STA absolute long 
-void opt8F (BYTE, BYTE, BYTE);
+void opt8F (BYTE i, BYTE j, BYTE k) {
+  STA(absoluteLong(i,j,k));
+}
 
 //BCC program counter relative
-void opt90 (BYTE);
+void opt90 (BYTE i) {
+  BCC(programCounterRelative(i));
+}
+
 //STA direct indirect indexed Y 
-void opt91 (BYTE);
+void opt91 (BYTE i) {
+  STA(directIndirectIndexed(i));
+}
+
 //STA direct indirect 
-void opt92 (BYTE);
+void opt92 (BYTE i) {
+  STA(directIndirect(i));
+}
+
 //STA stack realtive indirect indexed Y 
-void opt93 (BYTE);
+void opt93 (BYTE i) {
+  STA(stackRelativeIndirectIndexed(i));
+}
+
 //STY direct indexed X 
-void opt94 (BYTE);
+void opt94 (BYTE i) {
+  STY(directIndexedWithX(i));
+}
+
 //STA direct indexed X 
-void opt95 (BYTE);
+void opt95 (BYTE i) {
+  STA(directIndexedWithX(i));
+}
+
 //STX direct indexed Y 
-void opt96 (BYTE);
+void opt96 (BYTE i) {
+  STX(directIndexedWithY(i));
+}
+
 //STA direct indirect long indexed Y 
-void opt97 (BYTE);
+void opt97 (BYTE i) {
+  STA(directIndirectIndexedLong(i));
+}
+
 //TYA implied 
-void opt98 (void);
+void opt98 (void) {
+  TYA();
+}
+
 //STA absolute indexed Y 
-void opt99 (BYTE);
+void opt99 (BYTE i) {
+  STA(absoluteIndexedWithY(i));
+}
+
 //TXS impled 
-void opt9A (void);
+void opt9A (void) {
+  TXS();
+}
+
 //TXY implied 
-void opt9B (void);
+void opt9B (void) {
+  TXY();
+}
+
 //STZ absolute 
-void opt9C (BYTE, BYTE);
+void opt9C (BYTE i, BYTE j) {
+  STZ(absolute(i,j));
+}
+
 //STA Absolute indexed X 
-void opt9D (BYTE, BYTE);
+void opt9D (BYTE i, BYTE j) {
+  STA(absoluteIndexedWithX(i));
+}
+
 //STZ absolute indexed X
-void opt9E (BYTE, BYTE);
+void opt9E (BYTE i, BYTE j) {
+  STZ(absoluteIndexedWithX(i));
+}
+
 //STA absolute long indexed X 
-void opt9F (BYTE, BYTE, BYTE);
+void opt9F (BYTE i, BYTE j, BYTE k) {
+  STA(absoluteLongIndexedWithX(i,j,k));
+}
 
 //LDY immediate
-void optA0 (BYTE, BYTE);
+void optA0 (BYTE i, BYTE j);
+
 //LDA direct indexed indirect X
-void optA1 (BYTE);
+void optA1 (BYTE i) {
+  LDA(directIndexedIndirect(i));
+}
+
 //LDX immediate
-void optA2 (BYTE, BYTE);
+void optA2 (BYTE i, BYTE j);
+
 //LDA stack relative 
-void optA3 (BYTE);
+void optA3 (BYTE i) {
+  LDA(stackRelative(i));
+}
+
 //LDY direct 
-void optA4 (BYTE);
+void optA4 (BYTE i) {
+  LDY(direct(i));
+}
+
 //LDA direct 
-void optA5 (BYTE);
+void optA5 (BYTE i) {
+  LDA(direct(i));
+}
+
 //LDX direct 
-void optA6 (BYTE);
+void optA6 (BYTE i) {
+  LDX(direct(i));
+}
+
 //LDA direct indirect long 
-void optA7 (BYTE);
+void optA7 (BYTE i) {
+  LDA(directIndirectLong(i));
+}
+
 //TAY implied 
-void optA8 (void);
+void optA8 (void) {
+  TAY();
+}
+
 //LDA immediate 
-void optA9 (BYTE, BYTE);
+void optA9 (BYTE i, BYTE j);
+
 //TAX implied 
-void optAA (void);
+void optAA (void) {
+  TAX();
+}
+
 //PLB stack 
-void optAB (void);
+void optAB (void) {
+  PLB();
+}
+
 //LDY absolute
-void optAC (BYTE, BYTE);
+void optAC (BYTE i, BYTE j) {
+  LDY(absolute(i,j));
+}
+
 //LDA absolute 
-void optAD (BYTE, BYTE);
+void optAD (BYTE i, BYTE j) {
+  LDA(absolute(i,j));
+}
+
 //LDX absolute 
-void optAE (BYTE, BYTE);
+void optAE (BYTE i, BYTE j) {
+  LDX(absolute(i,j));
+}
+
 //LDA absolute long 
-void optAF (BYTE, BYTE, BYTE);
+void optAF (BYTE i, BYTE j, BYTE k) {
+  LDA(absoluteLong(i,j,k);)
+}
 
 //BCS program counter relative
-void optB0 (BYTE);
+void optB0 (BYTE i) {
+  BCS(programCounterRelative(i));
+}
+
 //LDA direct indirect indexed Y
-void optB1 (BYTE);
+void optB1 (BYTE i) {
+  LDA(directIndirectIndexed(i));
+}
+
 //LDA direct indirect 
-void optB2 (BYTE);
+void optB2 (BYTE i) {
+  LDA(directIndirect(i));
+}
+
 //LDA stack relative indirect indexed Y
-void optB3 (BYTE);
+void optB3 (BYTE i) {
+  LDA(stackRelativeIndirectIndexed(i));
+}
+
 //LDY direct indexed X
-void optB4 (BYTE);
+void optB4 (BYTE i) {
+  LDY(directIndexedWithX(i));
+}
+
 //LDA direct indexed X
-void optB5 (BYTE);
+void optB5 (BYTE i) {
+  LDA(directIndexedWithX(i));
+}
+
 //LDX direct indexed Y
-void optB6 (BYTE);
-//direct indirect long indexed Y
-void optB7 (BYTE);
+void optB6 (BYTE i) {
+  LDX(directIndexedWithY(i));
+}
+
+//LDA direct indirect long indexed Y
+void optB7 (BYTE i) {
+  LDA(directIndirectIndexedLong(i));
+}
+
 //CLV implied
-void optB8 (void);
+void optB8 (void) {
+  CLV();
+}
+
 //LDA absolute indexed Y
-void optB9 (BYTE, BYTE);
+void optB9 (BYTE i, BYTE j) {
+  LDA(absoluteIndexedWithY(i,j));
+}
+
 //TSX implied 
-void optBA (void);
+void optBA (void) {
+  TSX();
+}
+
 //TYX implied 
-void optBB (void);
+void optBB (void) {
+  TYX();
+}
+
 //LDY absolute indexed X
-void optBC (BYTE, BYTE);
+void optBC (BYTE i, BYTE j) {
+  LDY(absoluteIndexedWithX(i,j));
+}
+
 //LDA absolute indexed X
-void optBD (BYTE, BYTE);
+void optBD (BYTE i, BYTE j) {
+  LDA(absoluteIndexedWithX(i,j));
+}
+
 //LDX abolsute indexed Y
-void optBE (BYTE, BYTE);
+void optBE (BYTE i, BYTE j) {
+  LDA(absoluteIndexedWithY(i,j));
+}
+
 //LDA absolute long indexed X
-void optBF (BYTE, BYTE, BYTE);
+void optBF (BYTE i, BYTE j, BYTE k) {
+  LDA(absoluteLongIndexedWithX(i,j,k));
+}
--- optcodesC0-FF.c
+++ optcodesC0-FF.c
@@ -9,134 +9,303 @@
 #include "memory.h"
 
 //CPY immediate
-void optC0 (BYTE, BYTE);
+void optC0 (BYTE i, BYTE j);
+
 //CMP direct indexed indirect (X)
-void optC1 (BYTE);
+void optC1 (BYTE i) {
+  CMP(directIndexedIndirect(i));
+}
+
 //REP immediate 
-void optC2 (BYTE, BYTE);
+void optC2 (BYTE i, BYTE j);
+
 //CMP stack relative 
-void optC3 (BYTE);
+void optC3 (BYTE i) {
+  CMP(stackRelative(i));
+}
+
 //CPY direct 
-void optC4 (BYTE);
+void optC4 (BYTE i) {
+  CPY(direct(i));
+}
+
 //CMP direct 
-void optC5 (BYTE);
+void optC5 (BYTE i) {
+  CMP(direct(i));
+}
+
 //DEC direct 
-void optC6 (BYTE);
+void optC6 (BYTE i) {
+  DEC(direct(i));
+}
+
 //CMP direct indirect long
-void optC7 (BYTE);
+void optC7 (BYTE i) {
+  CMP(directIndirectLong(i));
+}
+
 //INY implied 
-void optC8 (void);
+void optC8 (void) {
+  INY();
+}
+
 //CMP immediate
-void optC9 (BYTE, BYTE);
+void optC9 (BYTE i, BYTE j);
+
 //DEX implied
-void optCA (void);
-//WAI implied 
+void optCA (void) {
+  DEX();
+}
+
+//WAI implied //TODO
 void optCB (void);
+
 //CPY absolute
-void optCC (BYTE, BYTE);
+void optCC (BYTE i, BYTE j) {
+  CPY(absolute(i,j));
+}
+
 //CMP absolute
-void optCD (BYTE, BYTE);
+void optCD (BYTE i, BYTE j) {
+  CMP(absolute(i,j));
+}
+
 //DEC absolute
-void optCE (BYTE, BYTE);
+void optCE (BYTE i, BYTE j) {
+  DEC(absolute(i,j));
+}
+
 //CMP absolute long
-void optCF (BYTE, BYTE, BYTE);
+void optCF (BYTE i, BYTE j, BYTE k) {
+  CMP(absoluteLong(i,j,k));
+}
 
 //BNE PC relative
-void optD0 (BYTE);
+void optD0 (BYTE i) {
+  BNE(programCounterRelative(i));
+}
+
 //CMP direct indirect indexed (Y)
-void optD1 (BYTE);
+void optD1 (BYTE i) {
+  CMP(directIndirectIndexed(i));
+}
+
 //CMP direct indirect
-void optD2 (BYTE);
+void optD2 (BYTE i) {
+  CMP(directIndirect(i));
+}
+
 //CMP stack relative indirect indexed (Y)
-void optD3 (BYTE);
+void optD3 (BYTE i) {
+  CMP(stackRelativeIndirectIndexed(i));
+}
+
 //PEI stack (direct indirect)
-void optD4 (BYTE);
+//TODO what happens here?
+void optD4 (BYTE i) {
+  PEI(directIndirect(i));
+}
+
 //CMP direct indexed (X)
-void optD5 (BYTE);
+void optD5 (BYTE i) {
+  CMP(directIndexedWithX(i));
+}
+
 //DEC direct indexed (X)
-void optD6 (BYTE);
+void optD6 (BYTE i) {
+  DEC(directIndexedWithX(i));
+}
+
 //CMP direct indirect long indexed (Y)
-void optD7 (BYTE);
+void optD7 (BYTE i) {
+  CMP(directIndirectIndexedLong(i));
+}
+
 //CLD implied
-void optD8 (void);
+void optD8 (void) {
+  CLD();
+}
+
 //CMP abolsute indexed (Y)
-void optD9 (BYTE, BYTE);
+void optD9 (BYTE i, BYTE j) {
+  CMP(absoluteIndexedWithY(i,j));
+}
+
 //PHX stack 
-void optDA (void);
-//STP implied 
+void optDA (void) {
+  PHX();
+}
+
+//STP implied //TODO
 void optDB (void);
+
 //JMP absolute indirect long
-void optDC (BYTE, BYTE);
+//TODO don't have along version? check instructions.c and jmp vs jml
+void optDC (BYTE i, BYTE j);
+
 //CMP absolute indexed (X)
-void optDD (BYTE, BYTE);
+void optDD (BYTE i, BYTE j) {
+  CMP(absoluteIndexedWithX(i,j));
+}
+
 //DEC absolute indexed (X)
-void optDE (BYTE, BYTE);
-//CMP absolute long indexed (X0
-void optDF (BYTE, BYTE, BYTE);
+void optDE (BYTE i, BYTE j) {
+  DEC(absoluteIndexedWithX(i,j));
+}
+
+//CMP absolute long indexed (X)
+void optDF (BYTE i, BYTE j, BYTE k) {
+  CMP(absoluteLongIndexedWithX(i,j,k));
+}
 
 //CPX immediate
-void optE0 (BYTE, BYTE);
+void optE0 (BYTE i, BYTE j);
+
 //SBC direct indexed indirect x 
-void optE1 (BYTE);
+void optE1 (BYTE i) {
+  SBC(directIndexedIndirect(i));
+}
+
 //SEP immediate 
-void optE2 (BYTE);
+void optE2 (BYTE i);
+
 //SBC stack relative 
-void optE3 (BYTE);
+void optE3 (BYTE i) {
+  SBC(stackRelative(i));
+}
+
 //CPX direct 
-void optE4 (BYTE);
+void optE4 (BYTE i) {
+  CMP(direct(i));
+}
+
 //SBC direct 
-void optE5 (BYTE);
+void optE5 (BYTE i) {
+  SBC(direct(i));
+}
+
 //INC direct 
-void optE6 (BYTE);
+void optE6 (BYTE i) {
+  INC(direct(i));
+}
+
 //SBC direct indirect long 
-void optE7 (BYTE);
+void optE7 (BYTE i) {
+  SBC(directIndirectLong(i));
+}
+
 //INX implied 
-void optE8 (void);
+void optE8 (void) {
+  INX();
+}
+
 //SBC immediate 
-void optE9 (BYTE, BYTE);
+void optE9 (BYTE i, BYTE j);
+
 //NOP implied
-void optEA (void);
+void optEA (void) {
+  NOP();
+}
+
 //XBA implied 
-void optEB (void);
+void optEB (void) {
+  XBA();
+}
+
 //CPX absolute
-void optEC (BYTE, BYTE);
+void optEC (BYTE i, BYTE j) {
+  CPX(absolute(i,j));
+}
+
 //SBC absolute 
-void optED (BYTE, BYTE);
+void optED (BYTE i, BYTE j) {
+  SBC(absolute(i,j));
+}
+
 //INC absolute
-void optEE (BYTE, BYTE);
+void optEE (BYTE i, BYTE j) {
+  INC(absolute(i.j));
+}
+
 //SBC absolute long 
-void optEF (BYTE, BYTE, BYTE);
+void optEF (BYTE i, BYTE j, BYTE k) {
+  SBC(absoluteLong(i,j,k));
+}
 
 //BEQ program counter relative
-void optF0 (BYTE);
-//SBC direct indirect indexed Y
-void optF1 (BYTE);
-//SBC direct indirect 
-void optF2 (BYTE);
-//SBC stack relative indirect indexed Y 
-void optF3 (BYTE);
-//PEA (immediate) stack 
-void optF4 (BYTE, BYTE);
-//SBC direct indexed X
-void optF5 (BYTE);
-//INC direct indexed X
-void optF6 (BYTE);
-//SBC direct indirect long indexed Y
-void optF7 (BYTE);
-//SED implied 
-void optF8 (void);
-//SBC absolute indexed Y 
-void optF9 (BYTE, BYTE);
-//PLX stack 
-void optFA (void);
-//XCE implied 
-void optFB (void);
-//JSR absolute indexed indirect 
-void optFC (BYTE, BYTE);
-//SBC absoluted indexed 
-void optFD (BYTE, BYTE);
-//INC absolute indexed X
-void optFE (BYTE, BYTE);
-//SBC absolute long indexed X 
-void optFF (BYTE, BYTE, BYTE);
+void optF0 (BYTE i) {
+  BEQ(programCounterRelative(i));
+}
 
+//SBC direct indirect indexed Y
+void optF1 (BYTE i) {
+  SBC(directIndirectIndexed(i));
+}
+
+//SBC direct indirect 
+void optF2 (BYTE i) {
+  SBC(directIndirect(i));
+}
+
+//SBC stack relative indirect indexed Y 
+void optF3 (BYTE i) {
+  SBC(stackRelativeIndirectIndexed(i));
+}
+
+//PEA (immediate) stack //TODO this is a constant??
+void optF4 (BYTE i, BYTE j);
+
+//SBC direct indexed X
+void optF5 (BYTE i) {
+  SBC(directIndexedWithX(i));
+}
+
+//INC direct indexed X
+void optF6 (BYTE i) {
+  INC(directIndexedWithX(i));
+}
+
+//SBC direct indirect long indexed Y
+void optF7 (BYTE i) {
+  SBC(directIndirectIndexedLong(i));
+}
+
+//SED implied 
+void optF8 (void) {
+  SED();
+}
+
+//SBC absolute indexed Y 
+void optF9 (BYTE i, BYTE j) {
+  SBC(absoluteIndexedWithY(i,j));
+}
+
+//PLX stack 
+void optFA (void) {
+  PLX();
+}
+
+//XCE implied 
+void optFB (void) {
+  XCE();
+}
+
+//JSR absolute indexed indirect 
+void optFC (BYTE i, BYTE j) {
+  JSR(absoluteIndexedIndirect(i,j));
+}
+
+//SBC absoluted indexed X
+void optFD (BYTE i, BYTE j) {
+  SBC(absoluteIndexedWithX(i,j));
+}
+
+//INC absolute indexed X
+void optFE (BYTE i, BYTE j) {
+  INC(absoluteIndexedWithX(i,j));
+}
+
+//SBC absolute long indexed X 
+void optFF (BYTE i, BYTE j, BYTE k) {
+ SBC(absoluteLongIndexedWithX(i,j,k)); 
+}
